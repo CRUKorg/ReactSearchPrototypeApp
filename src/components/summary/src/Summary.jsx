d945ee0e6b216ec03e36d79cdf3e9e4e549e59d2
heylookalive <allister.price@gmail.com>
2016-09-02 11:09:18 +0100

    Update the summary to be as per requirements, bold the search term.

--- src/components/summary/src/Summary.jsx
+++ src/components/summary/src/Summary.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import sanitizeHtml from 'sanitize-html-react';
 import {
-  HitsStats,
-  SearchkitComponent
+  HitsStats
 } from "searchkit";
 
 /**
@@ -9,35 +9,67 @@ import {
  */
 import './../styles/summary.scss';
 
-/**
- * Define the custom callback which we use in hitstats.
- */
-const customHitStats = (props) => {
-  const {resultsFoundLabel, bemBlocks, hitsCount, timeTaken} = props
+const CRUKHitsStatsDisplay = (props:HitsStatsDisplayProps) => {
+  const {resultsFoundLabel, bemBlocks} = props
 
   /**
-   * Don't output a summary if there aren't any results.
+   * As we're going to straight output the string as HTML, sanitise it first
+   * but allowing
    */
-  if (hitsCount < 1) {
-    return null;
+  let sanitisedResultsFoundLabel = {
+    __html: sanitizeHtml(resultsFoundLabel, {
+      allowedTags: ['strong'],
+      allowedAttributes: []
+    })
   }
 
   return (
-    <div className="search-summary" role="alert">
-      <div className={bemBlocks.container("info")} data-qa="info">
-        Found {hitsCount} results!
-      </div>
+    <div className={bemBlocks.container()} data-qa="hits-stats">
+      <div className={bemBlocks.container("info")} data-qa="info" dangerouslySetInnerHTML={sanitisedResultsFoundLabel}/>
     </div>
   )
 }
 
 /**
- * Define the component.
+ * Extend the hitstats component so we can set the default string in a smarter
+ * way formatting for singular/plural results. We also grab the search query.
  */
-export default class CRUKSearchSummary extends SearchkitComponent {
+export default class CRUKSearchSummary extends HitsStats {
+  constructor(props) {
+    super(props);
+
+    /**
+     * Change the default string to be what we want.
+     */
+    this.translations = {
+      'hitstats.results_found': '{hitCount} search {resultsWord} for <strong>{searchTerms}</strong>'
+    }
+  }
+
+  /**
+   * Change the default BEM classes.
+   */
+  defineBEMBlocks() {
+    return {
+      container: (this.props.mod || 'search-summary')
+    }
+  }
+
   render() {
-    return <HitsStats
-      mod="search-summary"
-      component={customHitStats} />
+    const timeTaken = this.searchkit.getTime()
+    const hitsCount = this.searchkit.getHitsCount()
+    const props:HitsStatsDisplayProps = {
+      bemBlocks: this.bemBlocks,
+      translate: this.translate,
+      timeTaken: timeTaken,
+      hitsCount: hitsCount,
+      resultsFoundLabel: this.translate("hitstats.results_found", {
+        timeTaken: timeTaken,
+        hitCount: hitsCount,
+        searchTerms: this.getQuery().index.queryString,
+        resultsWord: hitsCount === 1 ? 'result' : 'results'
+      })
+    }
+    return React.createElement(CRUKHitsStatsDisplay, props)
   }
 }
