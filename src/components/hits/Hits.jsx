60d22de2620825e3963ddca351ff4adf76373590
heylookalive <allister.price@gmail.com>
2016-09-11 16:27:20 +0100

    Add smooth scrolling.

--- src/components/hits/Hits.jsx
+++ src/components/hits/Hits.jsx
@@ -7,6 +7,7 @@ import {
  * Import our custom highlight accessor which won't mulch the array of objects.
  */
 import CRUKHighlightAccessor from './CRUKHighlightAccessor.jsx'
+import CRUKHitsAccessor from './CRUKHitsAccessor.jsx'
 /**
  * And the rest.
  */
@@ -22,12 +23,34 @@ class CRUKSearchHits extends Hits {
   }
 
   componentWillMount() {
-    super.componentWillMount()
+    /**
+     * Don't use super() here to avoid adding in both hits accessor and our
+     * custom one.
+     */
+    this.searchkit = this._getSearchkit()
+    if(this.searchkit){
+      this.accessor  = this.defineAccessor()
+      if(this.accessor){
+        this.accessor = this.searchkit.addAccessor(this.accessor)
+      }
+      this.stateListenerUnsubscribe = this.searchkit.emitter.addListener(()=> {
+        !this.unmounted && this.forceUpdate()
+      })
+    } else {
+      console.warn("No searchkit found in props or context for " + this.constructor["name"])
+    }
 
+    /**
+     * Add in the custom highlighter accessor which lets us specify exact
+     * settings per highlight field.
+     */
     if (this.props.CRUKHighlightFields) {
       this.searchkit.addAccessor(
         new CRUKHighlightAccessor(this.props.CRUKHighlightFields))
     }
+
+    this.hitsAccessor = new CRUKHitsAccessor({ scrollTo:this.props.scrollTo })
+    this.searchkit.addAccessor(this.hitsAccessor)
   }
 }
 
